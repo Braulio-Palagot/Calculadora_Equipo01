a4d8d189d7876e1c40b79b161d200bbdf2903426
Ceffyl_GitKraken <bdpalagot@gmail.com>
2022-03-24 10:43:28 -0600

    ADD Cambio de Base
    Cambios realizados:
        1) Se agregó la funcionalidad de cambio de base a 16, 8 y 2.

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,11 +1,14 @@
-window.onload = function(){ //Acciones tras cargar la página
-    pantalla=document.getElementById("textoPantalla"); //elemento pantalla de salida
+window.onload = function () { //Acciones tras cargar la página
+    pantalla = document.getElementById("textoPantalla"); //elemento pantalla de salida
+    $(".avanzada").hide();
 }
-x="0"; //número en pantalla
-xi=1; //iniciar número en pantalla: 1=si; 0=no;
-coma=0; //estado coma decimal 0=no, 1=si;
-ni=0; //número oculto o en espera.
-op="no"; //operación en curso; "no" =  sin operación.
+x = "0"; //número en pantalla
+valorDecimal = 0;
+valorFlotante = 0.0;
+xi = 1; //iniciar número en pantalla: 1=si; 0=no;
+coma = 0; //estado coma decimal 0=no, 1=si;
+ni = 0; //número oculto o en espera.
+op = "no"; //operación en curso; "no" =  sin operación.
 
 //mostrar número en pantalla según se va escribiendo:
 function numero(xx) { //recoge el número pulsado en el argumento.
@@ -20,15 +23,15 @@ function numero(xx) { //recoge el número pulsado en el argumento.
             x=xx; //guardar número
             coma=1; //cambiar estado de la coma
         }
-    }
-    else { //continuar escribiendo un número
-        if (xx=="." && coma==0) { //si escribimos una coma decimal pòr primera vez
-            pantalla.innerHTML+=xx;
-            x+=xx;
-            coma=1; //cambiar el estado de la coma
+    } else { //continuar escribiendo un número
+        if (xx == "." && coma == 0) { //si escribimos una coma decimal pòr primera vez
+            pantalla.innerHTML += xx;
+            x += xx;
+            coma = 1; //cambiar el estado de la coma
         }
         //si intentamos escribir una segunda coma decimal no realiza ninguna acción.
-        else if (xx=="." && coma==1) {}
+        else if (xx == "." && coma == 1) {
+        }
         //Resto de casos: escribir un número del 0 al 9:
         else {
             if (x == 2.718281 || xx == 'e') {
@@ -40,72 +43,83 @@ function numero(xx) { //recoge el número pulsado en el argumento.
             }
         }
     }
-    xi=0 //el número está iniciado y podemos ampliarlo.
-}
-function operar(s) {
-    igualar() //si hay operaciones pendientes se realizan primero
-    ni=x //ponemos el 1º número en "numero en espera" para poder escribir el segundo.
-    op=s; //guardamos tipo de operación.
-    xi=1; //inicializar pantalla.
-}
-function igualar() {
-    if (op=="no") { //no hay ninguna operación pendiente.
-        pantalla.innerHTML=x;	//mostramos el mismo número
-    }
-    else { //con operación pendiente resolvemos
-        sl=ni+op+x; // escribimos la operación en una cadena
-        sol=eval(sl) //convertimos la cadena a código y resolvemos
-        pantalla.innerHTML=sol //mostramos la solución
-        x=sol; //guardamos la solución
-        op="no"; //ya no hay operaciones pendientes
-        xi=1; //se puede reiniciar la pantalla.
-    }
-}
-function raizc() {
-    x=Math.sqrt(x) //resolver raíz cuadrada.
-    pantalla.innerHTML=x; //mostrar en pantalla resultado
-    op="no"; //quitar operaciones pendientes.
-    xi=1; //se puede reiniciar la pantalla
-}
-function porcent() {
-    x=x/100 //dividir por 100 el número
-    pantalla.innerHTML=x; //mostrar en pantalla
-    igualar() //resolver y mostrar operaciones pendientes
-    xi=1 //reiniciar la pantalla
-}
-function opuest() {
-    nx=Number(x); //convertir en número
-    nx=-nx; //cambiar de signo
-    x=String(nx); //volver a convertir a cadena
-    pantalla.innerHTML=x; //mostrar en pantalla.
-}
-function inve() {
-    nx=Number(x);
-    nx=(1/nx);
-    x=String(nx);
-    pantalla.innerHTML=x;
-    xi=1; //reiniciar pantalla al pulsar otro número.
+    xi = 0 //el número está iniciado y podemos ampliarlo.
 }
 
-function retro(){ //Borrar sólo el último número escrito.
-    cifras=x.length; //hayar número de caracteres en pantalla
-    br=x.substr(cifras-1,cifras) //describir último caracter
-    x=x.substr(0,cifras-1) //quitar el ultimo caracter
-    if (x=="") {x="0";} //si ya no quedan caracteres, pondremos el 0
-    if (br==".") {coma=0;} //Si el caracter quitado es la coma, se permite escribirla de nuevo.
-    pantalla.innerHTML=x; //mostrar resultado en pantalla
+function operar(s) {
+    igualar() //si hay operaciones pendientes se realizan primero
+    ni = x //ponemos el 1º número en "numero en espera" para poder escribir el segundo.
+    op = s; //guardamos tipo de operación.
+    xi = 1; //inicializar pantalla.
 }
+
+function igualar() {
+    if (op == "no") { //no hay ninguna operación pendiente.
+        pantalla.innerHTML = x;	//mostramos el mismo número
+    } else { //con operación pendiente resolvemos
+        sl = ni + op + x; // escribimos la operación en una cadena
+        sol = eval(sl) //convertimos la cadena a código y resolvemos
+        pantalla.innerHTML = sol //mostramos la solución
+        x = sol; //guardamos la solución
+        op = "no"; //ya no hay operaciones pendientes
+        xi = 1; //se puede reiniciar la pantalla.
+    }
+}
+
+function raizc() {
+    x = Math.sqrt(x) //resolver raíz cuadrada.
+    pantalla.innerHTML = x; //mostrar en pantalla resultado
+    op = "no"; //quitar operaciones pendientes.
+    xi = 1; //se puede reiniciar la pantalla
+}
+
+function porcent() {
+    x = x / 100 //dividir por 100 el número
+    pantalla.innerHTML = x; //mostrar en pantalla
+    igualar() //resolver y mostrar operaciones pendientes
+    xi = 1 //reiniciar la pantalla
+}
+
+function opuest() {
+    nx = Number(x); //convertir en número
+    nx = -nx; //cambiar de signo
+    x = String(nx); //volver a convertir a cadena
+    pantalla.innerHTML = x; //mostrar en pantalla.
+}
+
+function inve() {
+    nx = Number(x);
+    nx = (1 / nx);
+    x = String(nx);
+    pantalla.innerHTML = x;
+    xi = 1; //reiniciar pantalla al pulsar otro número.
+}
+
+function retro() { //Borrar sólo el último número escrito.
+    cifras = x.length; //hayar número de caracteres en pantalla
+    br = x.substr(cifras - 1, cifras) //describir último caracter
+    x = x.substr(0, cifras - 1) //quitar el ultimo caracter
+    if (x == "") {
+        x = "0";
+    } //si ya no quedan caracteres, pondremos el 0
+    if (br == ".") {
+        coma = 0;
+    } //Si el caracter quitado es la coma, se permite escribirla de nuevo.
+    pantalla.innerHTML = x; //mostrar resultado en pantalla
+}
+
 function borradoParcial() {
-    pantalla.innerHTML=0; //Borrado de pantalla;
-    x=0; //Borrado indicador número pantalla.
-    coma=0;	//reiniciamos también la coma
+    pantalla.innerHTML = 0; //Borrado de pantalla;
+    x = 0; //Borrado indicador número pantalla.
+    coma = 0;	//reiniciamos también la coma
 }
+
 function borradoTotal() {
-    pantalla.innerHTML=0; //poner pantalla a 0
-    x="0"; //reiniciar número en pantalla
-    coma=0; //reiniciar estado coma decimal
-    ni=0 //indicador de número oculto a 0;
-    op="no" //borrar operación en curso.
+    pantalla.innerHTML = 0; //poner pantalla a 0
+    x = "0"; //reiniciar número en pantalla
+    coma = 0; //reiniciar estado coma decimal
+    ni = 0 //indicador de número oculto a 0;
+    op = "no" //borrar operación en curso.
 }
 
 function cuadrado(){
@@ -115,9 +129,32 @@ function cuadrado(){
     xi=1;   
 }
 
+$(".prog").click(function () {
+    if (coma == 0) {
+        valorDecimal = parseInt(x);
+        if (this.value == "Decimal")
+            pantalla.innerHTML = valorDecimal.toString(10);
+        else if (this.value == "Hexadecimal")
+            pantalla.innerHTML = valorDecimal.toString(16);
+        else if (this.value == "Octal")
+            pantalla.innerHTML = valorDecimal.toString(8);
+        else if (this.value == "Binario")
+            pantalla.innerHTML = valorDecimal.toString(2);
+    } else {
+        valorFlotante = parseFloat(x);
+        if (this.value == "Decimal")
+            pantalla.innerHTML = valorFlotante.toString(10);
+        else if (this.value == "Hexadecimal")
+            pantalla.innerHTML = "No se puede convertir un valor flotante";
+        else if (this.value == "Octal")
+            pantalla.innerHTML = "No se puede convertir un valor flotante";
+        else if (this.value == "Binario")
+            pantalla.innerHTML = "No se puede convertir un valor flotante";
+    }
+});
+
 //Esta es la funcion para llamar y declarar PI        
 function pi() {
     x = Math.PI;
     pantalla.innerHTML=x;
     }
-
